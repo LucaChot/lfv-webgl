4a1cbcf3c711de6fb25fc7fae571c6849224e86e
lucac-zilliqa <luca.choteborsky@outlook.com>
2023-11-17 18:51:00 +0000

    Reached stage as previous implementation

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+//const { glMatrix } = require("gl-matrix");
+
+
 // app.js
 let gl;
 let shaderProgram;
+
 let modelViewMatrix = glMatrix.mat4.create();
 let projectionMatrix = glMatrix.mat4.create();
+
 let cameraPosition = glMatrix.vec3.fromValues(0, 0, 5);
 let targetPosition = glMatrix.vec3.fromValues(0, 0, 0);
 let upVector = glMatrix.vec3.fromValues(0, 1, 0);
@@ -14,6 +19,17 @@ let distance = 5;
 let mouseDown = false;
 let lastMouseX = 0;
 let lastMouseY = 0;
+
+let N = glMatrix.vec4.fromValues(0,0,1,1);
+let wF = glMatrix.vec4.fromValues(0,0,1,1);
+
+
+let inverseModelViewMatrix = glMatrix.mat4.create();
+let inverseProjectionMatrix = glMatrix.mat4.create();
+
+let modelViewMatrix2 = glMatrix.mat4.create();
+let projectionMatrix2 = glMatrix.mat4.create();
+let camera2Position = glMatrix.vec3.fromValues(0, 0, 5);
 
 function initWebGL(canvas) {
     gl = canvas.getContext("webgl");
@@ -39,10 +55,14 @@ function compileShader(source, type) {
 
 function initShaders() {
     const vsSource = `
+        precision mediump float;
+
         attribute vec3 position;
+        attribute vec2 uv;
+
         uniform mat4 uModelViewMatrix;
         uniform mat4 uProjectionMatrix;
-        attribute vec2 uv;
+
         varying vec2 vUv;
 
         void main(void) {
@@ -52,10 +72,31 @@ function initShaders() {
     `;
 
     const fsSource = `
-        varying mediump vec2 vUv;
+        precision mediump float;
+
         uniform sampler2D sampler;
+        uniform mat4 V_k_i;
+        uniform mat4 P_k_i;
+        uniform mat4 V_i;
+        uniform mat4 P_i;
+
+        varying vec2 vUv;
+
+        // Camera properties
+        vec3 cam_pos = vec3(0.0, 0.0, 5.0);
+        vec3 lookAtPosition = vec3(0.0, 0.0, 0.0);
+        vec3 upDirection = vec3(0.0, 1.0, 0.0);
+  
         void main(void) {
-            gl_FragColor = texture2D(sampler, vUv);
+          vec4 p_k = vec4(vUv * 2.0 - 1.0, 0 , 1);
+
+          vec4 p_i = P_i * V_i * V_k_i * P_k_i * p_k;
+          vec2 uv = (p_i.xy + 1.0) / 2.0;
+          vec3 tex = vec3(0.0, 0.0, 0.0);
+          if (uv.x >= 0.0 && uv.x <= 1.0 && uv.y >= 0.0 && uv.y <= 1.0) {
+            tex = vec3(texture2D(sampler, uv).rgb);
+          }
+          gl_FragColor = vec4(tex, 1.0); 
         }
     `;
   
@@ -82,6 +123,16 @@ function initShaders() {
 
     shaderProgram.modelViewMatrixUniform = gl.getUniformLocation(shaderProgram, "uModelViewMatrix");
     shaderProgram.projectionMatrixUniform = gl.getUniformLocation(shaderProgram, "uProjectionMatrix");
+
+    shaderProgram.vkiUniform = gl.getUniformLocation(shaderProgram, "V_k_i");
+    shaderProgram.pkiUniform = gl.getUniformLocation(shaderProgram, "P_k_i");
+    gl.uniformMatrix4fv(shaderProgram.vkiUniform, false, inverseModelViewMatrix);
+    gl.uniformMatrix4fv(shaderProgram.pkiUniform, false, inverseProjectionMatrix);
+
+    shaderProgram.viUniform = gl.getUniformLocation(shaderProgram, "V_i");
+    shaderProgram.piUniform = gl.getUniformLocation(shaderProgram, "P_i");
+    gl.uniformMatrix4fv(shaderProgram.viUniform, false, modelViewMatrix2);
+    gl.uniformMatrix4fv(shaderProgram.piUniform, false, projectionMatrix2);
 
     // Create texture
     var texture = gl.createTexture( );
@@ -127,9 +178,38 @@ function initBuffers() {
     gl.vertexAttribPointer(shaderProgram.uvAttribute , 2, gl.FLOAT, false, 0, 0);
 }
 
+function create4dProj(viewMatrix){
+  const N_c_4 = glMatrix.vec4.create();
+  glMatrix.vec4.transformMat4(N_c_4, N, viewMatrix);
+  const N_c = glMatrix.vec3.create();
+  glMatrix.vec3.copy(N_c, N_c_4);
+
+  const wF_c_4 = glMatrix.vec4.create();
+  glMatrix.vec4.transformMat4(wF_c_4, wF, viewMatrix);
+  const wF_c = glMatrix.vec3.create();
+  glMatrix.vec3.copy(wF_c, wF_c_4);
+
+  return glMatrix.mat4.fromValues(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    N_c[0], N_c[1], N_c[2], -glMatrix.vec3.dot(N_c, wF_c),
+    0.0, 0.0, 1.0, 0.0
+    );
+}
+
 function initCamera() {
     glMatrix.mat4.lookAt(modelViewMatrix, cameraPosition, targetPosition, upVector);
+    glMatrix.mat4.invert(inverseModelViewMatrix, modelViewMatrix);
     glMatrix.mat4.perspective(projectionMatrix, Math.PI / 4, gl.canvas.width / gl.canvas.height, 0.1, 10);
+    
+    p1 = create4dProj(modelViewMatrix);
+    glMatrix.mat4.invert(inverseProjectionMatrix, p1);
+
+    glMatrix.mat4.lookAt(modelViewMatrix2, camera2Position, targetPosition, upVector);
+    p2 = create4dProj(modelViewMatrix2);
+    glMatrix.mat4.copy(projectionMatrix2, p2);
+
+
 }
 
 function handleMouseDown(event) {
@@ -168,7 +248,11 @@ function updateCamera() {
     cameraPosition[2] = targetPosition[2] + distance * Math.cos(angleX);
 
     glMatrix.mat4.lookAt(modelViewMatrix, cameraPosition, targetPosition, upVector);
+    glMatrix.mat4.invert(inverseModelViewMatrix, modelViewMatrix);
+    p1 = create4dProj(modelViewMatrix);
+    glMatrix.mat4.invert(inverseProjectionMatrix, p1);
 
+    console.log(inverseProjectionMatrix)
     render();
 }
 
@@ -184,6 +268,9 @@ function render() {
 
     gl.uniformMatrix4fv(shaderProgram.modelViewMatrixUniform, false, modelViewMatrix);
     gl.uniformMatrix4fv(shaderProgram.projectionMatrixUniform, false, projectionMatrix);
+
+    gl.uniformMatrix4fv(shaderProgram.vkiUniform, false, inverseModelViewMatrix);
+    gl.uniformMatrix4fv(shaderProgram.pkiUniform, false, inverseProjectionMatrix);
 
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
 }
